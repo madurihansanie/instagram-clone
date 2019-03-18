1d40c376c0ae5edc78b590e3a7515fe0fb24a3c8
Maduri, Hansanie <madurihansanie@gmail.com>
2019-03-18 10:13:36 +0530

    javascript syntaxes

--- initFile.js
+++ initFile.js
@@ -1,1 +1,11 @@
 console.log("Hello world");
+var amount=12.3;
+const TAX_RATE= 0.09;
+if (amount>50){
+    console.log("The tax is added");
+}
+else{
+    console.log("There is not tax");
+}
+for (var i=0;i<=9; i++)
+{console.log(i);}
