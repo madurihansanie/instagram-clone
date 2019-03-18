d3a984188c55e634964f27dcedfe551fd8f79543
Maduri, Hansanie <madurihansanie@gmail.com>
2019-03-18 10:42:25 +0530

    exercises

---
+++ exercise.js
@@ -0,0 +1,17 @@
+// variables
+var a="find type ";
+var x= typeof a;
+console.log(x);
+// objects
+var obj= {a:"hello world", b:1, c:true};
+console.log(obj["a"]);
+console.log(obj["b"]);
+console.log(obj["c"]);
+//arrays 
+var arr= ["first index",2, false];
+console.log(arr[0]);
+function foo(){
+    var elem=9;
+    console.log(a + elem);
+}
+foo();
--- initFile.js
+++ initFile.js
@@ -5,7 +5,7 @@ if (amount>50){
     console.log("The tax is added");
 }
 else{
-    console.log("There is not tax");
+    console.log("There is no tax");
 }
 for (var i=0;i<=9; i++)
 {console.log(i);}
